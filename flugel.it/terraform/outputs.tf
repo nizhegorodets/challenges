c64b2d5bb2962a9d26c6bc89bcd7ea4d6ccb714b
Mikhail Kudimov <kudimov@Mikhails-MacBook-Pro.local>
2021-10-17 16:35:10 +0300

    sdfsdfsd

--- flugel.it/terraform/outputs.tf
+++ flugel.it/terraform/outputs.tf
@@ -1,4 +1,4 @@
 output "application_url" {
   description = "Public URI address of the python service EC2"
-  value = "http://${aws_instance.ec2_manager_service.public_dns}:8000"
+  value       = "http://${aws_instance.ec2_manager_service.public_dns}:8000"
 }
